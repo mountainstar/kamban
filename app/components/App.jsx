602db55ab13103cc28873a8f13d15c179e69664b
mountainstar <chriswirthdesign@gmail.com>
2016-10-11 21:41:02 -0600

    Implementing NoteStore and NoteActions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,30 +2,15 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            notes: [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Do laundry'
-                }
-            ]
-        }
-    }
     render() {
-        const {notes} = this.state;
+        const {notes} = this.props;
 
         return (
             <div>
-                {this.props.test}
                 <button className="add-note" onClick={this.addNote}>+</button>
                 <Notes
                     notes={notes}
@@ -37,56 +22,29 @@ class App extends React.Component {
         );
     }
     addNote = () => {
-        // It would be possible to write this in an imperative style.
-        // I.e., through `this.state.notes.push` and then
-        // `this.setState({notes: this.state.notes})` to commit.
-        //
-        // I tend to favor functional style whenever that makes sense.
-        // Even though it might take more code sometimes, I feel
-        // the benefits (easy to reason about, no side effects)
-        // more than make up for it.
-        //
-        // Libraries, such as Immutable.js, go a notch further.
-        this.setState({
-            notes: this.state.notes.concat([{
-                id: uuid.v4(),
-                task: 'New task'
-            }])
-        });
+      this.props.NoteActions.create({
+          id:uuid.v4(),
+          task: 'New task'
+      });
     }
     deleteNote = (id, e) => {
         // Avoid bubbling to edit
         e.stopPropagation();
 
-        this.setState({
-            notes: this.state.notes.filter(note => note.id !== id)
-        });
+
+        this.props.NoteActions.delete(id);
     }
     activateNoteEdit = (id) => {
-        this.setState({
-            notes: this.state.notes.map(note => {
-                if(note.id === id) {
-                    note.editing = true;
-                }
-
-                return note;
-            })
-        });
+        this.props.NoteActions.update({id, editing: true });
     }
     editNote = (id, task) => {
-        this.setState({
-            notes: this.state.notes.map(note => {
-                if(note.id === id) {
-                    note.editing = false;
-                    note.task = task;
-                }
-
-                return note;
-            })
-        });
+        const {NoteActions} = this.props;
+        NoteActions.update({id, task, editing: false});
     }
 }
 
-export default connect(() => ({
-    test: 'test'
-}))(App)
+export default connect(({notes}) => ({
+    notes
+}), {
+    NoteActions
+})(App)
---
+++ app/stores/NoteStore.js
@@ -0,0 +1,42 @@
+import uuid from 'uuid';
+import NoteActions from '../actions/NoteActions';
+
+
+export default class NoteStore {
+    constructor() {
+        this.bindActions(NoteActions);
+        this.notes = [
+            {
+                in: uuid.v4(),
+                task:'Learn React'
+            },
+            {
+                id: uuid.v4(),
+                task: 'Do laundry'
+            }
+        ];
+    }
+
+    create(note) {
+        this.setState({
+            notes: this.notes.concat(note)
+        });
+    }
+    update(updatedNote) {
+        this.setState({
+            notes: this.notes.map(note => {
+                if(note.id === updatedNote.id) {
+                    return Object.assign({}, note, updatedNote);
+                }
+                return note;
+            })
+        });
+    }
+    delete(id) {
+       this.setState({
+           notes: this.notes.filter(note => note.id !== id)
+       });
+    }
+}
+
+
